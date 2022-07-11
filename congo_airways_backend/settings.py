95c7aa1d26f1d033564b2d432135deadb149afaa
Mac <mac@MacBook-Pro-de-Mac.local>
2022-07-11 23:32:32 +0100

    five commit

--- congo_airways_backend/settings.py
+++ congo_airways_backend/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS= (os.path.join(BASE_DIR,'staticfiles'),)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
